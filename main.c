96fec05047fb11c1601103af7741a66326a1b20e
Daniel Neagu <danielgneagu@gmail.com>
2024-03-19 09:33:51 -0400

    major changes

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
-// Main file for Click Battle, an ECE243 Project 
+// Main file for Click Battle, an ECE243 Project
 // by Jonah Diamond & Daniel Neagu
 // Created 03/18/24
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 bool click();
 bool start();
 
-
-int main(void){
-    while (1){
-        while (!start()){}
-        while (!game()){
-            
-        }
+int main(void) {
+  while (1) {
+    while (!start()) {
     }
+    while (!game()) {
+    }
+  }
 
+  return 0;
+}
 
-
-    return 0;
+bool click() {
+  if (true) {
+    return false;
+  }
+  return true;
 }
