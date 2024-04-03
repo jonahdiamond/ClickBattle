2d40047ecbb686256d1cd7648049bdaaf64bf718
Daniel Neagu <danielgneagu@gmail.com>
2024-04-03 05:32:07 -0400

    fixed the start screen transition so the game starts after both players press their keys

--- main.c
+++ main.c
@@ -37,6 +37,7 @@ int abs(int x);
 void draw_line(int x0, int y0, int x1, int y1, short int line_color);
 void wait_for_sync();
 void draw_box(int x, int y, int width, short int colour);
+void draw_rect(int x, int y, int width, int height, short int colour);
 // header methods
 int pixel_buffer_start;       // global variable
 short int Buffer1[240][512];  // 240 rows, 512 (320 + padding) columns
@@ -97,16 +98,25 @@ int main(void) {
   int keydata = 0;
   *(PS2_ptr) = 0xFF;  // resets the input
 
+  bool key_released_break = false;
+  int key_released;
+  int old_gameLoc = gameLoc;
+  short int colour = 0;
+
   while (1) {
     xWins = 146;
     yWins = 120;
     bool Rwon = false;
-    // draw all loading screen messages
     startScreen();
     drawCLICKBATTLE();
-    drawBToStart();
     drawRToStart();
-    // switch buffers
+    drawBToStart();
+    wait_for_sync();
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1);
+    startScreen();
+    drawCLICKBATTLE();
+    drawRToStart();
+    drawBToStart();
     wait_for_sync();
     pixel_buffer_start = *(pixel_ctrl_ptr + 1);
     // flags for whether to display loading messages for r or b or to display
@@ -116,58 +126,46 @@ int main(void) {
     gameLoc = (SCREENLEFT + SCREENRIGHT) / 2;  // middle of the screen
     // loop for players readying
     while (!readyB || !readyR) {
-      // pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // switch back to back
-      // buffer
       PS2_data = *(PS2_ptr);       // read the Data register in the PS/2 port
       RVALID = PS2_data & 0x8000;  // extract the RVALID field
       if (RVALID) {
         keydata = PS2_data & 0xFF;  // extract which key clicked
-        if (keydata == Rkey) {
-          if (!readyR && readyB) {
-            readyR = true;
-            deleteRToStart();
-            deleteReadyB();
-          } else if (!readyR && !readyB) {
-            readyR = true;
-            deleteRToStart();
-            drawReadyR();
-          }
+        if (keydata == Rkey && !readyR) {
+          deleteRToStart();
+          drawReadyR();
+          wait_for_sync();
+          pixel_buffer_start = *(pixel_ctrl_ptr + 1);
+          deleteRToStart();
+          drawReadyR();
+          readyR = true;
+        } else if (keydata == Bkey && !readyB) {
+          deleteBToStart();
+          drawReadyB();
+          wait_for_sync();
+          pixel_buffer_start = *(pixel_ctrl_ptr + 1);
+          deleteBToStart();
+          drawReadyB();
+          readyB = true;
         }
-        if (keydata == Bkey) {
-          if (!readyB && readyR) {
-            readyB = true;
-            deleteBToStart();
-            deleteReadyR();
-          } else if (!readyB && !readyR) {
-            readyB = true;
-            deleteBToStart();
-            drawReadyB();
-          }
-        }
-        *(PS2_ptr) = 0xFF;  // resets the input
-        // wait_for_sync();
-        // pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // switch back to back
+        wait_for_sync();
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // switch back to back
         // buffer
       }
-      // wait_for_sync();
-      // pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // switch back to back
-      // buffer
     }
     // potentially show 3 2 1 go message with internal clock
     // delete start screen messages
     deleteCLICKBATTLE();
     wait_for_sync();
-    pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // switch back to back buffer
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1);
+    deleteCLICKBATTLE();
+    wait_for_sync();
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1);
+    // one small bug here.. the input break code and then key code are still in
+    // the FIFO and get read by the gameplay loop so it adds an extra time right
+    // as it loads in moving the screen over for one of the players
 
     // actual gameplay loop - in game if gameLoc is in bounds of screen,
     // otherwise game over
-
-    bool key_released_break = false;
-    int key_released;
-    int old_gameLoc = gameLoc;
-    short int colour = 0;
-    short int b_colour = 0xf2ff;
-    short int d_colour = 0x3596;
 
     while (gameLoc > SCREENLEFT && gameLoc < SCREENRIGHT) {
       PS2_data = *(PS2_ptr);       // read the Data register in the PS/2 port
@@ -186,10 +184,10 @@ int main(void) {
         old_gameLoc = gameLoc;
         if (key_released == Bkey) {
           gameLoc = gameLoc + 1;
-          colour = b_colour;
+          colour = red;
         } else if (key_released == Rkey) {
           gameLoc = gameLoc - 1;
-          colour = d_colour;
+          colour = blue;
         }
         key_released = 0;
       }
@@ -331,6 +329,12 @@ void draw_box(int x, int y, int width, short int colour) {
     draw_line(x, y + i, x + width, y + i, colour);
   }
 }
+
+void draw_rect(int x, int y, int width, int height, short int colour) {
+  for (int i = 0; i < height; i++) {
+    draw_line(x, y + i, x + width, y + i, colour);
+  }
+}
 void wait_for_sync() {  // waits for V-sync and rendering to finish
   volatile int* pixel_ctrl_ptr = (int*)0xFF203020;
   int status;
@@ -399,269 +403,6 @@ void clear_screen() {
     }
   }
 }
-/*
-void drawCLICKBATTLE() {
-  // code for drawing a C
-  // top of C
-  draw_line(54, 30, 69, 30, 0xffff);
-  draw_line(54, 31, 69, 31, 0xffff);
-  // upper curve of C
-  draw_line(54, 30, 50, 38, 0xffff);
-  draw_line(54, 31, 50, 39, 0xffff);
-  // left line of C
-  draw_line(50, 38, 50, 50, 0xffff);
-  draw_line(51, 38, 51, 50, 0xffff);
-  // lower curve of C
-  draw_line(50, 50, 54, 58, 0xffff);
-  draw_line(51, 50, 54, 58, 0xffff);
-  // lower line of C
-  draw_line(54, 58, 69, 58, 0xffff);
-  draw_line(54, 59, 69, 59, 0xffff);
-
-  // code for drawing an L
-  // vertical line
-  draw_line(74, 30, 74, 59, 0xffff);
-  draw_line(75, 30, 75, 59, 0xffff);
-  // horizontal line
-  draw_line(74, 58, 90, 58, 0xffff);
-  draw_line(74, 59, 90, 59, 0xffff);
-
-  // code for drawing an I
-  draw_line(95, 30, 95, 59, 0xffff);
-  draw_line(96, 30, 96, 59, 0xffff);
-
-  // code for drawing a C
-  // top of C
-  draw_line(105, 30, 120, 30, 0xffff);
-  draw_line(105, 31, 120, 31, 0xffff);
-  // upper curve of C
-  draw_line(105, 30, 101, 38, 0xffff);
-  draw_line(105, 31, 101, 39, 0xffff);
-  // left line of C
-  draw_line(101, 38, 101, 50, 0xffff);
-  draw_line(102, 38, 102, 50, 0xffff);
-  // lower curve of C
-  draw_line(101, 50, 105, 58, 0xffff);
-  draw_line(102, 50, 105, 58, 0xffff);
-  // lower line of C
-  draw_line(105, 58, 120, 58, 0xffff);
-  draw_line(105, 59, 120, 59, 0xffff);
-
-  // code for drawing a K
-  // vertical line
-  draw_line(125, 30, 125, 59, 0xffff);
-  draw_line(126, 30, 126, 59, 0xffff);
-  // upper diagonal
-  draw_line(125, 45, 135, 30, 0xffff);
-  draw_line(126, 45, 136, 30, 0xffff);
-  // lower diagonal
-  draw_line(126, 45, 136, 59, 0xffff);
-  draw_line(125, 45, 135, 59, 0xffff);
-
-  // code for drawing a B
-  // vertical
-  draw_line(162 + 22, 30, 162 + 22, 59, 0xffff);
-  draw_line(163 + 22, 30, 163 + 22, 59, 0xffff);
-  // upper curve
-  draw_line(162 + 22, 30, 167 + 22, 30, 0xffff);
-  draw_line(162 + 22, 31, 167 + 22, 31, 0xffff);
-  draw_line(167 + 22, 30, 172 + 22, 36, 0xffff);
-  draw_line(167 + 22, 31, 172 + 22, 37, 0xffff);
-  draw_line(172 + 22, 36, 172 + 22, 41, 0xffff);
-  draw_line(173 + 22, 36, 173 + 22, 41, 0xffff);
-  draw_line(171 + 22, 41, 166 + 22, 45, 0xffff);
-  draw_line(173 + 22, 41, 167 + 22, 45, 0xffff);
-  // lower curve
-  draw_line(167 + 22, 45, 172 + 22, 51, 0xffff);
-  draw_line(167 + 22, 46, 172 + 22, 52, 0xffff);
-  draw_line(172 + 22, 51, 172 + 22, 56, 0xffff);
-  draw_line(173 + 22, 51, 173 + 22, 56, 0xffff);
-  draw_line(172 + 22, 56, 166 + 22, 59, 0xffff);
-  draw_line(173 + 22, 56, 167 + 22, 59, 0xffff);
-  draw_line(162 + 22, 58, 167 + 22, 58, 0xffff);
-  draw_line(162 + 22, 59, 167 + 22, 59, 0xffff);
-
-  // code for drawing an A
-  // left diagonal
-  draw_line(177 + 22, 59, 184 + 22, 30, 0xffff);
-  draw_line(178 + 22, 59, 185 + 22, 30, 0xffff);
-  // right diagonal
-  draw_line(191 + 22, 59, 184 + 22, 30, 0xffff);
-  draw_line(192 + 22, 59, 185 + 22, 30, 0xffff);
-  // middle line
-  draw_line(181 + 22, 44, 188 + 22, 44, 0xffff);
-  draw_line(181 + 22, 45, 188 + 22, 45, 0xffff);
-
-  // code for drawing a T
-  // horizontal
-  draw_line(195 + 22, 30, 205 + 22, 30, 0xffff);
-  draw_line(195 + 22, 31, 205 + 22, 31, 0xffff);
-  // vertical
-  draw_line(200 + 22, 30, 200 + 22, 59, 0xffff);
-  draw_line(201 + 22, 30, 201 + 22, 59, 0xffff);
-
-  // code for drawing a T
-  // horizontal
-  draw_line(210 + 22, 30, 220 + 22, 30, 0xffff);
-  draw_line(210 + 22, 31, 220 + 22, 31, 0xffff);
-  // vertical
-  draw_line(215 + 22, 30, 215 + 22, 59, 0xffff);
-  draw_line(216 + 22, 30, 216 + 22, 59, 0xffff);
-
-  // code for drawing an L
-  // vertical line
-  draw_line(225 + 22, 30, 225 + 22, 59, 0xffff);
-  draw_line(226 + 22, 30, 226 + 22, 59, 0xffff);
-  // horizontal line
-  draw_line(225 + 22, 58, 241 + 22, 58, 0xffff);
-  draw_line(225 + 22, 59, 241 + 22, 59, 0xffff);
-
-  // code for drawing an E
-  // vertical line
-  draw_line(246 + 22, 30, 246 + 22, 59, 0xffff);
-  draw_line(247 + 22, 30, 247 + 22, 59, 0xffff);
-  // bottom line
-  draw_line(246 + 22, 58, 262 + 22, 58, 0xffff);
-  draw_line(246 + 22, 59, 262 + 22, 59, 0xffff);
-  // top line
-  draw_line(246 + 22, 30, 262 + 22, 30, 0xffff);
-  draw_line(246 + 22, 31, 262 + 22, 31, 0xffff);
-  // middle line
-  draw_line(246 + 22, 44, 262 + 22, 44, 0xffff);
-  draw_line(246 + 22, 45, 262 + 22, 45, 0xffff);
-}
-
-void deleteCLICKBATTLE() {
-  // code for drawing a C
-  // top of C
-  draw_line(54, 30, 69, 30, red);
-  draw_line(54, 31, 69, 31, red);
-  // upper curve of C
-  draw_line(54, 30, 50, 38, red);
-  draw_line(54, 31, 50, 39, red);
-  // left line of C
-  draw_line(50, 38, 50, 50, red);
-  draw_line(51, 38, 51, 50, red);
-  // lower curve of C
-  draw_line(50, 50, 54, 58, red);
-  draw_line(51, 50, 54, 58, red);
-  // lower line of C
-  draw_line(54, 58, 69, 58, red);
-  draw_line(54, 59, 69, 59, red);
-
-  // code for drawing an L
-  // vertical line
-  draw_line(74, 30, 74, 59, red);
-  draw_line(75, 30, 75, 59, red);
-  // horizontal line
-  draw_line(74, 58, 90, 58, red);
-  draw_line(74, 59, 90, 59, red);
-
-  // code for drawing an I
-  draw_line(95, 30, 95, 59, red);
-  draw_line(96, 30, 96, 59, red);
-
-  // code for drawing a C
-  // top of C
-  draw_line(105, 30, 120, 30, red);
-  draw_line(105, 31, 120, 31, red);
-  // upper curve of C
-  draw_line(105, 30, 101, 38, red);
-  draw_line(105, 31, 101, 39, red);
-  // left line of C
-  draw_line(101, 38, 101, 50, red);
-  draw_line(102, 38, 102, 50, red);
-  // lower curve of C
-  draw_line(101, 50, 105, 58, red);
-  draw_line(102, 50, 105, 58, red);
-  // lower line of C
-  draw_line(105, 58, 120, 58, red);
-  draw_line(105, 59, 120, 59, red);
-
-  // code for drawing a K
-  // vertical line
-  draw_line(125, 30, 125, 59, red);
-  draw_line(126, 30, 126, 59, red);
-  // upper diagonal
-  draw_line(125, 45, 135, 30, red);
-  draw_line(126, 45, 136, 30, red);
-  // lower diagonal
-  draw_line(126, 45, 136, 59, red);
-  draw_line(125, 45, 135, 59, red);
-
-  // code for drawing a B
-  // vertical
-  draw_line(162 + 22, 30, 162 + 22, 59, blue);
-  draw_line(163 + 22, 30, 163 + 22, 59, blue);
-  // upper curve
-  draw_line(162 + 22, 30, 167 + 22, 30, blue);
-  draw_line(162 + 22, 31, 167 + 22, 31, blue);
-  draw_line(167 + 22, 30, 172 + 22, 36, blue);
-  draw_line(167 + 22, 31, 172 + 22, 37, blue);
-  draw_line(172 + 22, 36, 172 + 22, 41, blue);
-  draw_line(173 + 22, 36, 173 + 22, 41, blue);
-  draw_line(171 + 22, 41, 166 + 22, 45, blue);
-  draw_line(173 + 22, 41, 167 + 22, 45, blue);
-  // lower curve
-  draw_line(167 + 22, 45, 172 + 22, 51, blue);
-  draw_line(167 + 22, 46, 172 + 22, 52, blue);
-  draw_line(172 + 22, 51, 172 + 22, 56, blue);
-  draw_line(173 + 22, 51, 173 + 22, 56, blue);
-  draw_line(172 + 22, 56, 166 + 22, 59, blue);
-  draw_line(173 + 22, 56, 167 + 22, 59, blue);
-  draw_line(162 + 22, 58, 167 + 22, 58, blue);
-  draw_line(162 + 22, 59, 167 + 22, 59, blue);
-
-  // code for drawing an A
-  // left diagonal
-  draw_line(177 + 22, 59, 184 + 22, 30, blue);
-  draw_line(178 + 22, 59, 185 + 22, 30, blue);
-  // right diagonal
-  draw_line(191 + 22, 59, 184 + 22, 30, blue);
-  draw_line(192 + 22, 59, 185 + 22, 30, blue);
-  // middle line
-  draw_line(181 + 22, 44, 188 + 22, 44, blue);
-  draw_line(181 + 22, 45, 188 + 22, 45, blue);
-
-  // code for drawing a T
-  // horizontal
-  draw_line(195 + 22, 30, 205 + 22, 30, blue);
-  draw_line(195 + 22, 31, 205 + 22, 31, blue);
-  // vertical
-  draw_line(200 + 22, 30, 200 + 22, 59, blue);
-  draw_line(201 + 22, 30, 201 + 22, 59, blue);
-
-  // code for drawing a T
-  // horizontal
-  draw_line(210 + 22, 30, 220 + 22, 30, blue);
-  draw_line(210 + 22, 31, 220 + 22, 31, blue);
-  // vertical
-  draw_line(215 + 22, 30, 215 + 22, 59, blue);
-  draw_line(216 + 22, 30, 216 + 22, 59, blue);
-
-  // code for drawing an L
-  // vertical line
-  draw_line(225 + 22, 30, 225 + 22, 59, blue);
-  draw_line(226 + 22, 30, 226 + 22, 59, blue);
-  // horizontal line
-  draw_line(225 + 22, 58, 241 + 22, 58, blue);
-  draw_line(225 + 22, 59, 241 + 22, 59, blue);
-
-  // code for drawing an E
-  // vertical line
-  draw_line(246 + 22, 30, 246 + 22, 59, blue);
-  draw_line(247 + 22, 30, 247 + 22, 59, blue);
-  // bottom line
-  draw_line(246 + 22, 58, 262 + 22, 58, blue);
-  draw_line(246 + 22, 59, 262 + 22, 59, blue);
-  // top line
-  draw_line(246 + 22, 30, 262 + 22, 30, blue);
-  draw_line(246 + 22, 31, 262 + 22, 31, blue);
-  // middle line
-  draw_line(246 + 22, 44, 262 + 22, 44, blue);
-  draw_line(246 + 22, 45, 262 + 22, 45, blue);
-}
-*/
 
 void drawCLICKBATTLE() {
   int counter = 0;
@@ -676,21 +417,8 @@ void drawCLICKBATTLE() {
   }
 }
 void deleteCLICKBATTLE() {
-  int counter = 0;
-  for (int j = 0; j < 24; j++) {
-    for (int i = 0; i < 46; i++) {
-      // short int value = ((pcsmall_map[counter]&0xFF)<<8) |
-      // ((pcsmall_map[counter]&0xFF00)>>8);
-      // short int value = CLICKBATTLE[counter];
-      short int value;
-      if (i < 23)
-        value = red;
-      else
-        value = blue;
-      plot_pixel(i + 137, j + 50, value);
-      counter++;
-    }
-  }
+  draw_rect(0, 0, 159, 240, red);
+  draw_rect(160, 0, 159, 240, blue);
 }
 void drawBToStart() {
   int counter = 0;
